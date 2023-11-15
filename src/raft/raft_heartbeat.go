6612445e6473ab6b92a12fca0e12f747d54980ad
Yitong Wu <wuyt26@outlook.com>
2023-11-15 00:48:50 -0800

    3a snapshot optimization

--- src/raft/raft_heartbeat.go
+++ src/raft/raft_heartbeat.go
@@ -5,39 +5,53 @@ func (rf *Raft) broadcastAppendEntries() {
 		return
 	}
 
+	var snapshotData []byte
+	var isSnapshotNeeded bool
 	for i := range rf.peers {
-		if i != rf.me {
-			if rf.nextIndex[i] <= rf.lastIncludedIndex {
-				snapshotArgs := InstallSnapshotArgs{
-					Term:              rf.currentTerm,
-					LeaderId:          rf.me,
-					LastIncludedIndex: rf.lastIncludedIndex,
-					LastIncludedTerm:  rf.lastIncludedTerm,
-					Data:              rf.persister.ReadSnapshot(),
-				}
-				snapshotReply := InstallSnapshotReply{}
-				go rf.sendInstallSnapshot(i, &snapshotArgs, &snapshotReply)
-				continue
-			}
-
-			args := AppendEntriesArgs{}
-			args.Term = rf.currentTerm
-			args.LeaderId = rf.me
-			args.LeaderCommit = rf.commitIndex
-			args.PrevLogIndex = rf.nextIndex[i] - 1
-
-			if args.PrevLogIndex == rf.lastIncludedIndex {
-				args.PrevLogTerm = rf.lastIncludedTerm
-			} else {
-				args.PrevLogTerm = rf.log[rf.getRelativeIndex(args.PrevLogIndex)].Term
-			}
-
-			args.Entries = make([]LogEntry, 0)
-			args.Entries = append(args.Entries, rf.log[rf.getRelativeIndex(args.PrevLogIndex+1):]...)
-
-			appendEntriesReply := AppendEntriesReply{}
-			go rf.sendAppendEntries(i, &args, &appendEntriesReply)
-
+		if i != rf.me && rf.nextIndex[i] <= rf.lastIncludedIndex {
+			isSnapshotNeeded = true
+			break
 		}
+	}
+
+	if isSnapshotNeeded {
+		snapshotData = rf.persister.ReadSnapshot()
+	}
+
+	for i := range rf.peers {
+		if i == rf.me {
+			continue
+		}
+
+		if rf.nextIndex[i] <= rf.lastIncludedIndex {
+			snapshotArgs := InstallSnapshotArgs{
+				Term:              rf.currentTerm,
+				LeaderId:          rf.me,
+				LastIncludedIndex: rf.lastIncludedIndex,
+				LastIncludedTerm:  rf.lastIncludedTerm,
+				Data:              snapshotData,
+			}
+			snapshotReply := InstallSnapshotReply{}
+			go rf.sendInstallSnapshot(i, &snapshotArgs, &snapshotReply)
+			continue
+		}
+
+		args := AppendEntriesArgs{}
+		args.Term = rf.currentTerm
+		args.LeaderId = rf.me
+		args.LeaderCommit = rf.commitIndex
+		args.PrevLogIndex = rf.nextIndex[i] - 1
+
+		if args.PrevLogIndex == rf.lastIncludedIndex {
+			args.PrevLogTerm = rf.lastIncludedTerm
+		} else {
+			args.PrevLogTerm = rf.log[rf.getRelativeIndex(args.PrevLogIndex)].Term
+		}
+
+		args.Entries = make([]LogEntry, 0)
+		args.Entries = append(args.Entries, rf.log[rf.getRelativeIndex(args.PrevLogIndex+1):]...)
+
+		appendEntriesReply := AppendEntriesReply{}
+		go rf.sendAppendEntries(i, &args, &appendEntriesReply)
 	}
 }
